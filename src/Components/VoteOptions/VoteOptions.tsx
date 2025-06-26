b8ce1c51f9fd435f878ab3d0c9d434dbe1e75313
Vadim Klus <vadim178963@gmail.com>
2025-06-26 18:24:32 +0300

    addallapp

--- src/Components/VoteOptions/VoteOptions.tsx
+++ src/Components/VoteOptions/VoteOptions.tsx
@@ -1,12 +1,36 @@
-import css from "./VoteOptions.module.css";
+import styles from "./VoteOptions.module.css";
+import type { VoteType } from "../../types/votes";
 
-export default function VoteOptions() {
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteOptionsProps) {
   return (
-    <div className={css.container}>
-      <button className={css.button}>Good</button>
-      <button className={css.button}>Neutral</button>
-      <button className={css.button}>Bad</button>
-      <button className={`${css.button} ${css.reset}`}>Reset</button>
+    <div className={styles.container}>
+      <button className={styles.button} onClick={() => onVote("good")}>
+        Good
+      </button>
+      <button className={styles.button} onClick={() => onVote("neutral")}>
+        Neutral
+      </button>
+      <button className={styles.button} onClick={() => onVote("bad")}>
+        Bad
+      </button>
+      {canReset && (
+        <button
+          className={`${styles.button} ${styles.reset}`}
+          onClick={onReset}
+        >
+          Reset
+        </button>
+      )}
     </div>
   );
 }
