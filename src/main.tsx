f52fe956fc5cc2b43f772056676da8b65093dfc8
Vadim Klus <vadim178963@gmail.com>
2025-06-26 19:10:28 +0300

    fixMain

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
+import App from "./components/App/App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
